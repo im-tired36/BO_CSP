111860d85b12c7e12bd45bb45d410ce730a3a1a4
im-tired36 <bliss.oh@ucas-edu.net>
2026-09-09 13:52:54 -0600

    variable notes

---
+++ variables_notes.py
@@ -0,0 +1,7 @@
+# VL, Variables Notes
+
+name = input("What is your name?")
+
+print ("Homework is a pain...")
+print("Mental breakdowns are normal now.")
+print("Hello", name, "its going to be great!")
