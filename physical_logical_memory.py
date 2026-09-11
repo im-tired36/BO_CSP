fe44df4ac9b95e80191150e1f668132a3c67c91e
im-tired36 <bliss.oh@ucas-edu.net>
2026-09-11 13:38:51 -0600

    more notes

---
+++ physical_logical_memory.py
@@ -0,0 +1,8 @@
+# BO, physical and logical memory
+
+pi= "3.14159"
+
+everything = 42
+
+print(f"the variable pi is {pi} and it is saved at {id(pi)}")
+print(f" the variable everything is {everything} and it is saved at {id(everything)}")
