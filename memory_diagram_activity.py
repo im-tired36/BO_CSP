369606494cf4828caa0efcecf784f7eb20459790
im-tired36 <bliss.oh@ucas-edu.net>
2026-09-14 13:35:52 -0600

    memorydiagram

---
+++ memory_diagram_activity.py
@@ -0,0 +1,27 @@
+# BO, 6th, 
+
+name = "bob"
+age = "95"
+
+print(f"the variable name is saved at {id(name)}")
+print(f"the variable age is saved at {id(age)}")
+
+score = "10"
+print(f"the variable score is saved at {id(score)}")
+
+score = "25"
+print(f"the variable score is saved at {id(score)}")
+
+height = "5.3"
+city = "seoul"
+zip_code = "64986"
+
+print(f"the variable height is saved at {id(height)}")
+print(f"the variable city is saved at {id(city)}")
+print(f"the variable zip_code is saved at {id(zip_code)}")
+
+price1 = "10"
+price2 = "10"
+
+print(f"the variable price1 is saved at {id(price1)}")
+print(f"the variable price2 is saved at {id(price2)}")
