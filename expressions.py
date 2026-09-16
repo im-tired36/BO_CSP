d3b0db19a9829857e99aa58c6282a0fe0ef3d7f0
im-tired36 <bliss.oh@ucas-edu.net>
2026-09-16 13:53:16 -0600

    expressions

---
+++ expressions.py
@@ -0,0 +1,22 @@
+#BO, 6th, Integer, Floats and Expressionss Notes
+
+# Integers
+people = 18
+shoes = people * 2
+hand = people * 2
+
+#Float
+pi = 3.14159
+water = 60.8
+temp = 68.8
+price  = 1.99
+
+#expression
+print(f"your {water} of water costs ${round(water*price,2)}")
+
+#modulo and integer division
+print(f"this is the result of 18%3 {18%3}")
+print(f"18/4 = {18//4} with a remainder of {18%4}")
+print(f"18/5 = {18/6} which is {18//6} with a remainder of {18%6}")
+
+print(int(people*2*(5-1)/3))
