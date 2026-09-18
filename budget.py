d296aa462db7fd3df4c6277d65f4aec6e5c90b60
im-tired36 <bliss.oh@ucas-edu.net>
2026-09-18 11:50:24 -0600

    budget2

--- budget.py
+++ budget.py
@@ -35,9 +35,12 @@ while True:
         break
     except:
         print("That isn't a number.")
+saving = income* 0.10
+spending_money = income - rent_mortgage - utilities - groceries - transportation - saving
 
-print(f"The cost of your rent/mortgage is {rent_mortgage} and {rent_mortgage/income*100,2}")
-print()
-print()
-print()
-print()
+print(f"The cost of your rent/mortgage is {rent_mortgage} and {rent_mortgage/income*100}")
+print(f"The cost of your utilities is {utilities} and {utilities/income*100}% of your income")
+print(f"The cost of your is {groceries} and {groceries/income*100}% of your income")
+print(f"The cost of your is {transportation} and {transportation/income*100}% of your imcome")
+print(f"you should save ${saving} and thats 10% of your income.")
+print(f"${spending_money} is your spending money.")
