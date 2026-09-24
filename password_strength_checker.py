d01c2a98832d1a090f5a5c04e4bcf9ad0e7c7cad
im-tired36 <bliss.oh@ucas-edu.net>
2026-09-24 13:19:54 -0600

    passwordstrength

--- password_strength_checker.py
+++ password_strength_checker.py
@@ -6,11 +6,11 @@ uppercase = False
 lowercase = False
 number = False
 symbol = False
+count = 0
+strength = "weak"
 
 if len(password) >= 8:
     length = True
-else:
-    print(f"At least 8 characters: {length}")
 print(f"At least 8 character: {length}")
 
 for letter in password:
@@ -28,5 +28,29 @@ for letter in password:
         number = True
 print(f"Has a number: {number}")
 
-for letter in "!@#$%^&*();:?/<>,.`~-_=+":
-    symbol = True
+for letter in password:
+    if letter in "!@#$%^&*();:?/<>,.`~-_=+":
+         symbol = True
+print(f"Has a symbol: {symbol}")
+
+if length == True:
+    count = count + 1 
+if uppercase == True:
+    count = count + 1 
+if lowercase == True:
+    count = count + 1 
+if number == True:
+    count = count + 1 
+if symbol == True:
+    count = count + 1 
+
+if count == 5:
+    strength = "strong"
+if count <= 4:
+    strength = "meduim"
+if count < 3:
+    strength = "weak"
+
+print(f"You have a {strength} password.")
+print(f"You have done {count}/5.")
+print("If you don't have a 5/5, you should check if you have a uppercase letter, lowercase letter, a number, and a symbol.")
